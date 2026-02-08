d814480d17b773285bfc5b994c6d27494d40c80a
sandeep kumar mamloda <sandeepmamloda06@gmail.com>
2026-02-08 22:38:06 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
 import Stairs from './components/common/Stairs.jsx'
 import NavContext from './context/NavContext.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Stairs>
         <NavContext>
           <App />
         </NavContext>
       </Stairs>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
